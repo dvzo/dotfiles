6ef4d812283f09a09cf9aaea9368f1ed0ea6d777
dvzo <dillonvicente@gmail.com>
2023-08-17 19:57:20 -1000

    adding hyprland files

--- .update-dotfiles.sh
+++ .update-dotfiles.sh
@@ -3,10 +3,13 @@
 echo "updating update script..."
 cp ~/.update-dotfiles.sh ~/src/dotfiles/
 
-
 echo "updating dotfiles..."
 
 # copy i3 files to git repo "dotfiles"
-cp -r ~/.config/i3/ ~/src/dotfiles/
+# temporarily moving from i3 to hyprland
+# cp -r ~/.config/i3/ ~/src/dotfiles/
+
+# copy hyprland files to git repo "dotfiles"
+cp -r ~/.config/hypr/ ~/src/dotfiles/
 
 echo "finished updating scripts and files!"
