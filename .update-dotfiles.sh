8dd06a2c774476c7ad097b65d8232b58de50f341
dvzo <dillonvicente@gmail.com>
2023-08-18 16:33:45 -1000

    adding eww scripts, updating dotfiles

--- .update-dotfiles.sh
+++ .update-dotfiles.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+#
+echo "updating dotfiles!"
 
 echo "updating update script..."
 cp ~/.update-dotfiles.sh ~/src/dotfiles/
@@ -6,23 +8,21 @@ cp ~/.update-dotfiles.sh ~/src/dotfiles/
 echo "updating xdg toggle portals script..."
 cp ~/.toggle-xdg-portals.sh ~/src/dotfiles/
 
-echo "updating dotfiles..."
-
-# copy i3 files to git repo "dotfiles"
-# temporarily moving from i3 to hyprland
+# echo "updating i3 files..."
+# moving from i3 to hyprland
 # cp -r ~/.config/i3/ ~/src/dotfiles/
 
-echo "copying hyprland files..."
+echo "updating hyprland files..."
 # copy hyprland files to git repo "dotfiles"
 cp -r ~/.config/hypr/ ~/src/dotfiles/
 
-echo "copying waybar files..."
+echo "updating waybar files..."
 cp -r /etc/xdg/waybar ~/src/dotfiles/
 
-echo "copying eww files..."
+echo "updating eww files..."
 cp -r ~/.config/eww/ ~/src/dotfiles/
 
-echo "copying vimrc..."
+echo "updating vimrc..."
 cp ~/.vimrc ~/src/dotfiles/
 
 echo "finished updating scripts and files!"
